a72fbe362e9cf65524042083547a649c1192d15f
VALENNE Nathan <nathan.valenne@gmail.com>
2023-03-01 00:51:20 +0100

    implements models
    TODO : musics + resize models

--- src/components/Renderer3D.vue
+++ src/components/Renderer3D.vue
@@ -1,12 +1,14 @@
 <template>
+  <div>
     <div id="container"></div>
-  </template>
+    <div>{{modele}}</div>
+  </div>
+</template>
 
 <script type="module">
   import * as Three from 'three'
   import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
   let scene = new Three.Scene();
-  let model;
   export default {
     name: "Renderer3D",
     data() {
@@ -18,36 +20,53 @@
     },
     methods: {
       init: function() {
+        /**
+         * Create scene
+         * @type {HTMLElement}
+         */
         let container = document.getElementById('container');
+        this.camera = new Three.PerspectiveCamera( 120, window.innerWidth / window.innerHeight );
+        this.camera.position.set(0, 2, 5.5);
+        scene.add(new Three.AmbientLight(0xFFFFFF, 1));
 
-        this.camera = new Three.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.4, 1500 );
-        this.camera.position.z = 2.5;
-        this.camera.position.y = 1;
-
+        /**
+         * Loading 3D model
+         * @type {GLTFLoader}
+         */
         const loader = new GLTFLoader();
-        loader.load( `models/capybara/scene.gltf`, function ( gltf ) {
-          model = gltf;
-          scene.add( gltf.scene );
+        const urlModel = `models/${this.modele}/scene.gltf`;
+        let model;
+        loader.load( urlModel, function ( gltf ) {
+          model = gltf.scene;
+          model.scale.set(0.8,0.8,0.8)
+          scene.add(model);
         }, undefined, function ( error ) {
           console.error( error );
         } );
 
-        this.renderer = new Three.WebGLRenderer({antialias: true});
-        this.renderer.setSize(window.innerWidth/2, window.innerHeight/2);
+        /**
+         * Rendering scene
+         * @type {WebGLRenderer}
+         */
+        this.renderer = new Three.WebGLRenderer({alpha: true});
+        this.renderer.setSize(window.innerWidth/1.3, window.innerHeight/1.3);
         container.appendChild(this.renderer.domElement);
 
       },
       animate: function() {
         requestAnimationFrame(this.animate);
 
-        model.scene.rotation.y += 0.01;
-
+        // Rotation animation
+        scene.rotation.y += 0.03;
         this.renderer.render(scene, this.camera);
-      }
+      },
     },
     mounted() {
       this.init();
       this.animate();
+    },
+    props : {
+      modele : String
     }
   }
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,32 @@
 <template>
   <div class="main-container">
     <div>
-      <select class="select_models models">
+      <select class="select_models models" v-model="model_selected" v-if="!canBeGenerated">
         <optgroup label="Modèles">
           <option value="capybara">Capybara</option>
           <option value="maxwell">Maxwell dance</option>
           <option value="floppa_cube">Floppa</option>
+          <option value="el_gato">El Gato</option>
+          <option value="gangnam_style">Gangnam Style</option>
         </optgroup>
       </select>
-      <select class="select_models musics">
+      <select class="select_models musics" v-model="music_selected" v-if="!canBeGenerated">
         <optgroup label="Musiques">
-          <!-- c'est nous les daft punk -->
-          <option value="around the world">around the world</option>
+          <option value="around_the_world">Around The World</option> <!-- c'est nous les daft punk -->
+          <option value="ok_i_pull_up">Ok I Pull Up</option>
         </optgroup>
       </select>
     </div>
-    <Renderer3D/>
-    <div class="container">
-      feur
+    <div v-if="!canBeGenerated">
+      <button class="btn-generate" v-if="model_selected && music_selected && !canBeGenerated" @click="changeStateGeneration">Generate</button>
     </div>
+    <div v-else>
+      <button class="btn-generate" v-if="canBeGenerated" @click="changeStateGeneration">Back</button>
+    </div>
+    <Renderer3D :modele="model_selected" v-if="canBeGenerated" />
+    <a href="https://marrantmaispastrop.fun/feur" target="_blank" style="text-decoration: none;color: black">
+      quoi
+    </a>
   </div>
 </template>
 
@@ -30,6 +38,26 @@ import Renderer3D from "@/components/Renderer3D.vue";
     components: {
       Renderer3D
     },
+    data() {
+      return {
+        music_selected : '',
+        model_selected : '',
+        canBeGenerated : false
+      }
+    },
+    methods : {
+      playMusic(){
+        this.music_playing = new Audio(`./musics/${this.music_selected}.mp3`)
+        this.music_playing.play();
+        this.music_playing.loop = true;
+      },
+      stopMusic(){
+        this.music_playing.stop();
+      },
+      changeStateGeneration(){
+        this.canBeGenerated=!this.canBeGenerated;
+      },
+    }
   }
 </script>
 
@@ -40,7 +68,7 @@ import Renderer3D from "@/components/Renderer3D.vue";
     align-items: center;
     justify-content: center;
   }
-  .musics, .models {
+  .musics, .models, .btn-generate {
     margin: 10px;
     background: black;
     color: white;
