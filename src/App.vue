10f089afa9cbf0a3698ffea47c2dd14f4f95a9a2
VALENNE Nathan <nathan.valenne@gmail.com>
2023-02-28 09:00:37 +0100

    add navbar + module three

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,33 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+      <NavBar/>
+      <router-view/>
   </div>
 </template>
 
+<script>
+
+import NavBar from "@/components/NavBar.vue";
+
+export default {
+  name: 'App',
+  components: {NavBar},
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+  #app {
+    display: flex;
+    flex-flow: column wrap;
+    overflow: hidden;
+    width: 100%;
+  }
+  body {
+    max-width: 100%;
+    margin: 0;
+  }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,50 @@
+<template>
+  <nav class="navbar">
+    <ul class="navbar-list">
+      <li>
+        <router-link to="/">Home</router-link>
+      </li>
+      <li>
+        <router-link to="/about">About</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+}
+</script>
+
+<style scoped>
+  ul {
+    list-style-type: none;
+  }
+
+  .navbar {
+    background: black;
+    height: 80px;
+    font-family: "Climate Crisis", "sans-serif";
+    width: 100%;
+  }
+
+  .navbar-list {
+    display: flex;
+    flex-direction: row;
+    align-content: center;
+    justify-content: center;
+  }
+  .navbar-list li {
+    margin: 0 10px 0 10px;
+    color: white;
+    font-size: 24px;
+  }
+  a {
+    text-decoration: none;
+    color: white;
+  }
+  .navbar-list a.router-link-exact-active {
+    color: #42b983;
+  }
+</style>
